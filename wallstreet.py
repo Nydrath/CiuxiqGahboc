d54c2d8b4e7f42f0f523c8dba03512b3f7539461
Nydrath <mark.t.dewl@gmail.com>
2017-02-07 23:42:03 +0100

    Added discord code.

--- wallstreet.py
+++ wallstreet.py
@@ -1,6 +1,7 @@
 import urllib.request
 import json
 import os
+import discord
 
 def direction(x):
     if x > 0:
@@ -10,20 +11,36 @@ def direction(x):
     else:
         return "→"
 
-response = urllib.request.urlopen("https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,LTC,ETH,XMR,MAID,DASH,EDR,ZEC,NXT,XRP&tsyms=USD")
-try:
-    data = json.loads(str(response.read(), 'utf-8'))
-except:
-    print(response.read())
+def querynumbers():
+    response = urllib.request.urlopen("https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,LTC,ETH,XMR,MAID,DASH,EDR,ZEC,NXT,XRP&tsyms=USD")
+    try:
+        data = json.loads(str(response.read(), 'utf-8'))
+    except:
+        print(response.read())
 
-if os.path.exists("oldvalues.json"):
-    with open("oldvalues.json", "r") as f:
-        olddata = json.loads(f.read())
-else:
-    olddata = data.copy()
+    if os.path.exists("oldvalues.json"):
+        with open("oldvalues.json", "r") as f:
+            olddata = json.loads(f.read())
+    else:
+        olddata = data.copy()
 
-for key, value in sorted(data.items()):
-    print(key, direction(value["USD"]-olddata[key]["USD"]), value["USD"])
+    result = ""
+    for key, value in sorted(data.items()):
+        result += "{0} {1} {2}\n".format(key, direction(value["USD"]-olddata[key]["USD"]), value["USD"])
 
-with open("oldvalues.json", "w") as f:
-    f.write(json.dumps(data))
+    with open("oldvalues.json", "w") as f:
+        f.write(json.dumps(data))
+
+    return result
+
+
+discordclient = discord.Client()
+
+@discordclient.event
+@asyncio.coroutine
+def on_message(message):
+    if discordclient.user.mention in message.content and not message.author.bot and message.channel == "#wallstreet":
+        try:
+            yield from discordclient.send_message(message.channel, querynumbers())
+        except discord.errors.Forbidden:
+            pass
